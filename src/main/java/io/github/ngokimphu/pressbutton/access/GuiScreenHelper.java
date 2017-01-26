ea45e116d04f46654eb34828462ff85224dc9ad6
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-01-26 22:26:59 +0800

    Add helper to press button with separate enchanting handling

---
+++ src/main/java/io/github/ngokimphu/pressbutton/access/GuiScreenHelper.java
@@ -0,0 +1,66 @@
+package io.github.ngokimphu.pressbutton.access;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.List;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiButton;
+import net.minecraft.client.gui.GuiEnchantment;
+import net.minecraft.client.gui.GuiScreen;
+import net.minecraft.inventory.Container;
+
+public final class GuiScreenHelper {
+    private static Field buttonList;
+    private static Method actionPerformed;
+
+    static {
+        try {
+            buttonList = GuiScreen.class.getDeclaredField("buttonList");
+            buttonList.setAccessible(true);
+            actionPerformed = GuiScreen.class.getDeclaredMethod("actionPerformed", GuiButton.class);
+            actionPerformed.setAccessible(true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static int screenButtonClick(int buttonNumber, int mouse) {
+        try {
+            GuiScreen screen = Minecraft.getMinecraft().currentScreen;
+            if (screen == null) {
+                return -1;
+            }
+            if (screen instanceof GuiEnchantment) {
+                if (buttonNumber > 2) {
+                    return Integer.MAX_VALUE;
+                }
+                Container enchantment = ((GuiEnchantment) screen).inventorySlots;
+                if (enchantment.enchantItem(Minecraft.getMinecraft().thePlayer, buttonNumber)) {
+                    Minecraft.getMinecraft().playerController.sendEnchantPacket(enchantment.windowId, buttonNumber);
+                    return 0;
+                }
+                return -1;
+            }
+            List<GuiButton> buttonList = getButtonList(screen);
+            if (buttonNumber >= buttonList.size()) {
+                return Integer.MAX_VALUE;
+            }
+            GuiButton button = buttonList.get(buttonNumber);
+            if (!button.mousePressed(Minecraft.getMinecraft(), button.xPosition, button.yPosition)) {
+                return -1;
+            }
+            actionPerformed.invoke(screen, button);
+            return 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return -2;
+        }
+    }
+
+    @SuppressWarnings("unchecked")
+    public static List<GuiButton> getButtonList(GuiScreen screen) throws Exception {
+        return (List<GuiButton>) buttonList.get(screen);
+    }
+
+}
