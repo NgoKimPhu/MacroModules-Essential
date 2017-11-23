c6d193a4d865614d2df1b1cb26237f4f8c6d0adb
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2017-11-23 19:25:21 +0800

    Add slotmiddleclick module to middle-click an inventory slot

---
+++ src/main/java/io/github/ngokimphu/slotmiddleclick/actions/ScriptActionSlotMiddleClick.java
@@ -0,0 +1,72 @@
+package io.github.ngokimphu.slotmiddleclick.actions;
+
+import io.github.ngokimphu.ModuleInfo;
+import io.github.ngokimphu.slotmiddleclick.util.SlotClicker;
+import net.eq2online.macros.scripting.api.APIVersion;
+import net.eq2online.macros.scripting.api.IMacro;
+import net.eq2online.macros.scripting.api.IMacroAction;
+import net.eq2online.macros.scripting.api.IReturnValue;
+import net.eq2online.macros.scripting.api.IScriptActionProvider;
+import net.eq2online.macros.scripting.parser.ScriptAction;
+import net.eq2online.macros.scripting.parser.ScriptContext;
+import net.eq2online.macros.scripting.parser.ScriptCore;
+
+/**
+ * slotmiddleclick action to middle click a slot. Syntax:
+ * slotmiddleclick(slotnumber)
+ */
+@APIVersion(ModuleInfo.API_VERSION)
+public class ScriptActionSlotMiddleClick extends ScriptAction {
+    protected final SlotClicker slotClicker;
+
+    public ScriptActionSlotMiddleClick() {
+        // Context is the context for this action, action name must be lowercase
+        this(ScriptContext.MAIN);
+    }
+
+    public ScriptActionSlotMiddleClick(ScriptContext context) {
+        super(context, "slotmiddleclick");
+        this.slotClicker = new SlotClicker(this.macros, this.mc);
+    }
+
+    /**
+     * This action cannot run on the network thread, eg. it is invalid in chat
+     * filter scripts or onSendChatMessage handlers
+     *
+     * @see net.eq2online.macros.scripting.parser.ScriptAction#isThreadSafe()
+     */
+    @Override
+    public boolean isThreadSafe() {
+        return false;
+    }
+
+    /**
+     * Execute the slotmiddleclick action
+     * 
+     * @param params 1 only parameter of slot number
+     * @see net.eq2online.macros.scripting.actions.game.ScriptActionSlotClick#execute
+     * @see net.eq2online.macros.scripting.parser.ScriptAction#execute
+     */
+    @Override
+    public IReturnValue execute(IScriptActionProvider provider, IMacro macro, IMacroAction instance,
+            String rawParams, String[] params) {
+        if (params.length > 0) {
+            int slotNumber = ScriptCore.tryParseInt(provider.expand(macro, params[0], false), 0);
+            this.slotClicker.containerSlotMiddleClick(slotNumber);
+        }
+
+        return null;
+    }
+
+    /**
+     * Called after this action is initialized, the action should register with
+     * the script core.
+     * 
+     * @see net.eq2online.macros.scripting.parser.ScriptAction#onInit()
+     */
+    @Override
+    public void onInit() {
+        this.context.getCore().registerScriptAction(this);
+    }
+
+}
---
+++ src/main/java/io/github/ngokimphu/slotmiddleclick/util/SlotClicker.java
@@ -0,0 +1,116 @@
+package io.github.ngokimphu.slotmiddleclick.util;
+
+import net.eq2online.macros.compatibility.PrivateFields;
+import net.eq2online.macros.compatibility.PrivateFields.StaticFields;
+import net.eq2online.macros.core.Macros;
+import net.eq2online.macros.core.overlays.IContainerCreative;
+import net.eq2online.macros.core.overlays.IGuiContainer;
+import net.eq2online.macros.core.overlays.IGuiContainerCreative;
+import net.eq2online.macros.gui.helpers.SlotHelper;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.inventory.GuiContainer;
+import net.minecraft.client.gui.inventory.GuiContainerCreative;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.inventory.ClickType;
+import net.minecraft.inventory.Container;
+import net.minecraft.inventory.InventoryBasic;
+import net.minecraft.inventory.Slot;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.NonNullList;
+
+public class SlotClicker extends SlotHelper {
+    private final Minecraft mc;
+
+    public SlotClicker(Macros macros, Minecraft mc) {
+        super(macros, mc);
+        this.mc = mc;
+    }
+
+    public void containerSlotMiddleClick(int slotNumber) {
+        final int middleButton = this.mc.gameSettings.keyBindPickBlock.getKeyCode() + 100;
+        try {
+            if (!this.currentScreenIsContainer()) {
+                if (this.noScreenInGame() && slotNumber >= 1 && slotNumber <= 9) {
+                    this.mc.player.inventory.currentItem = slotNumber - 1;
+                }
+                return;
+            }
+
+            GuiContainer ex = this.getGuiContainer();
+            Container slots = ex.inventorySlots;
+            Slot slot;
+            if (ex instanceof GuiContainerCreative) {
+                if (slotNumber < 45) {
+                    ((IGuiContainerCreative) ex).setCreativeTab(CreativeTabs.INVENTORY);
+                    slot = slotNumber < 5
+                            ? (Slot) PrivateFields.creativeBinSlot.get((GuiContainerCreative) ex)
+                            : slots.getSlot(slotNumber);
+                } else if (slotNumber < 54) {
+                    slot = slots.getSlot(slotNumber);
+                } else {
+                    if (slotNumber > 599 && slotNumber < 700) {
+                        return;
+                    }
+
+                    int slot2 = slotNumber / 100 - 1;
+                    ((IGuiContainerCreative) ex).setCreativeTab(CreativeTabs.CREATIVE_TAB_ARRAY[slot2]);
+                    slotNumber -= (slot2 + 1) * 100;
+                    if (!scrollContainerTo((GuiContainerCreative) ex, slotNumber)) {
+                        return;
+                    }
+                    slotNumber -= getCreativeInventoryScroll((GuiContainerCreative) ex);
+                    slot = slots.getSlot(slotNumber);
+                }
+                ((IGuiContainerCreative) ex).mouseClick(slot, slotNumber, middleButton, ClickType.CLONE);
+            } else if (slotNumber >= 0 && slotNumber < slots.inventorySlots.size() || slotNumber == -999) {
+                slot = slotNumber == -999 ? null : slots.getSlot(slotNumber);
+                ((IGuiContainer) ex).mouseClick(slot, slotNumber, middleButton, ClickType.CLONE);
+            }
+        } catch (Exception arg7) {
+            arg7.printStackTrace();
+        }
+
+    }
+
+    private boolean noScreenInGame() {
+        return this.mc.currentScreen == null && this.mc.player != null && this.mc.player.inventory != null
+                && this.mc.player.inventory.mainInventory != null;
+    }
+
+    private static boolean scrollContainerTo(GuiContainerCreative containerGui, int slotNumber)
+            throws IllegalArgumentException, SecurityException {
+        float currentScroll = 0.0F;
+        int lastInventoryScroll = setScrollPosition(containerGui, currentScroll);
+        NonNullList<?> itemsList = ((IContainerCreative) containerGui.inventorySlots).getItemsList();
+        float scrollIncrement = (float) (1.0D / (itemsList.size() / 9 - 5 + 1));
+
+        int inventoryScroll;
+        do {
+            if (isInRange(slotNumber, getCreativeInventoryScroll(containerGui), 45)) {
+                return true;
+            }
+
+            currentScroll += scrollIncrement;
+            inventoryScroll = setScrollPosition(containerGui, currentScroll);
+        } while (inventoryScroll != lastInventoryScroll);
+
+        return isInRange(slotNumber, inventoryScroll, 45);
+    }
+
+    private static boolean isInRange(int value, int start, int rangeLength) {
+        return value >= start && value < start + rangeLength;
+    }
+
+    private static int getCreativeInventoryScroll(GuiContainerCreative containerGui) {
+        try {
+            InventoryBasic ex = StaticFields.creativeInventory.get();
+            ItemStack firstSlotStack = ex.getStackInSlot(0);
+            NonNullList<?> itemsList = ((IContainerCreative) containerGui.inventorySlots).getItemsList();
+            return itemsList.indexOf(firstSlotStack);
+        } catch (Exception arg3) {
+            arg3.printStackTrace();
+            return 0;
+        }
+    }
+
+}
