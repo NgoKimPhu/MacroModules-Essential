ae9547926d4b9122c457e1a91f683d2d78fcc332
NgoKimPhu <kimphu_2408@yahoo.com.vn>
2019-08-24 09:48:26 +0800

    move classes to new sub-package macromodules.essential

---
+++ src/main/java/io/github/ngokimphu/macromodules/essential/ModuleInfo.java
@@ -0,0 +1,19 @@
+package io.github.ngokimphu.macromodules.essential;
+
+/**
+ * Shared information for this module
+ */
+public abstract class ModuleInfo {
+
+    /**
+     * Target API version
+     */
+    public static final int API_VERSION = 26;
+
+    /**
+     * Private ctor, no instances
+     */
+    private ModuleInfo() {
+    }
+
+}
